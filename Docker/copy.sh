22b815bc99cb50ff6688dbe9c9d715d15430bdc6
Igor Gomes <i.greygomes3@outlook.com>
2022-05-25 19:23:09 -0300

    Update copy.sh

--- Docker/copy.sh
+++ Docker/copy.sh
@@ -8,3 +8,5 @@ sudo apt update && sudo apt upgrade
 echo "instalando a GUI"
 sudo apt-get install xrdp lxde-core lxde tigervnc-standalone-server -y
 
+echo "clonando repositorio"
+git clone https://github.com/Victor-Afonso/docker.git
